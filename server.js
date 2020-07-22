c5da500d339ccb557fd064efc69d0af086162402
deep1401 <gandhi0869@gmail.com>
2020-07-22 20:46:42 +0530

    Setup router

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bootcamps = require("./routes/bootcamps");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+app.use("/bootcamps/api/v1", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
