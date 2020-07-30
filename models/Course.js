1f534fa194a248fd2dad47709e19733f42dd7fa3
deep1401 <gandhi0869@gmail.com>
2020-07-30 16:35:12 +0530

    Cascading courses and average cost of bootcamps

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,41 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+//Creating statics
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log("Calculating average cost".blue.inverse);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(obj);
+};
+
+//Calculating average cost post save
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Calculating average cost pre remove
+CourseSchema.pre("remove", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
