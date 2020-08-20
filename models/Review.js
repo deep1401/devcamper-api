7e33bf67c4f14c873d42c3f3af419b106274b3a7
deep1401 <gandhi0869@gmail.com>
2020-08-20 20:43:52 +0530

    Calculate average rating of reviews

--- models/Review.js
+++ models/Review.js
@@ -35,4 +35,39 @@ const ReviewSchema = new mongoose.Schema({
 
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+//Creating statics
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  //console.log("Calculating average cost".blue.inverse);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//Calculating average cost post save
+ReviewSchema.post("save", function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+//Calculating average cost pre remove
+ReviewSchema.pre("remove", function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
